2c976cb7130b97f9d7ed8cf9534802da8666d302
Gericke <ghoeksema@thecodegroup.co.za>
2019-09-25 18:51:24 +0200

    -applying redux store with no actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Component
 import App from './components/App/App';
+
+// Style
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+
+// reducers
+import reducers from './reducers'
+
+const storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore); // High order function
+
+ReactDOM.render(
+    <Provider store={storeWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
